93b83cdcdcf80a87fd270789c9ff67c67f5f6010
Uriel Roman Carrizalez <ourcssdev@gmail.com>
2024-07-04 21:57:10 -0600

    header component with some features added

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      "white-smoke": "#F5F5F5",
+      "princeton-orange": "#F68F0E",
+      "smoky-black": "#0C0704",
+      "chocolate": "#814B09",
+      "sunset": "#F6C282",
+      "tigerseye": "#BC6D0C"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
